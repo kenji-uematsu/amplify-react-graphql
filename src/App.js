d185a7182eda81727d1d9de7c8353e7cc3842899
kenji-uematsu <FZF01113@nifty.com>
2025-04-06 09:00:50 +0900

    add authenticator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>Hello from V2</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "amplify_outputs.json";
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(outputs);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
